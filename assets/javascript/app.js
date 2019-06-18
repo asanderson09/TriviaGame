1923b1a3355487478619933b492de6c6d6f5fd4e
Anthony A <asanderson2009@gmail.com>
2019-06-17 20:50:58 -0400

    Questions added

---
+++ assets/javascript/app.js
@@ -0,0 +1,74 @@
+$(document).ready(function(){
+    $('#start').on('click', function(){
+        $('#container').remove();
+    })
+var questions = [{
+    question: 'What is the largest bone in the human body',
+    answers:["Jaw","Scapula",'Femur', 'Tibia'],
+    rightAnswer:'Femur'
+}, {
+    question: 'What is the largest bone in the human body?',
+    answers:["Jaw","Scapula",'Femur', 'Tibia'],
+    rightAnswer:'Femur'
+}, { 
+    question: 'Which pop album is the best selling of all time?',
+    answers:["M.J. Thriller","Prince Purple Rain",'The Beatles The Beatles'],
+    rightAnswer:'M.J. Thriller '
+}, {
+    question: 'What is the total number of dots on a pair of dice?',
+    answers:["50","42",'38', '40'],
+    rightAnswer:'42'
+}, {
+    question: 'Which part of the body would a chiropodist treat?',
+    answers:["The arm","The back",'The feet', 'The butt'],
+    rightAnswer:'The feet'
+}, {
+    question: 'How many rings make up the symbol of the Olympic Games?',
+    answers:["3","6",'4', '5'],
+    rightAnswer:'5'
+}, {
+    question: 'What is the colored part of the human eye called?',
+    answers:["Cornea","Iris",'Bulbus', 'Retina'],
+    rightAnswer:'Iris'
+}, {
+    question: 'What is a group of lions called?',
+    answers:["A pack","A mob",'A pride', 'A flock'],
+    rightAnswer:'A pride'
+}, {
+    question: 'The Statue of Liberty was a gift to commemorate what specific group?',
+    answers:["Military Troops","American Patriots",'Hippies', 'Slaves'],
+    rightAnswer:'Slaves'
+}] 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+});
