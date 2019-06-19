3ac49a82b22d57defb8de1f310483f840f567a78
Anthony A <asanderson2009@gmail.com>
2019-06-19 00:04:50 -0400

    Submitting what I have for HW and will finish it later

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,27 +1,12 @@
-$(document).ready(function () {
-    //Start button to kick everything off
-    $('#startButton').on('click', function () {
-        //removes start button once pressed
-        $('#startButton').remove();
-        // for loop that runs through the questions array
-        for (var i = 0; i < questions.length; i++) {
-            // Takes the question from the questions array and appends them displaying them in a header
-            $('#container').append('<h2>' + questions[i].question + '</h2>');
-            // for loop that runs through the answers from their respective questions index
-            for (var j = 0; j < questions[i].answers.length; j++) {
-                //  append each answer with a radio type,  a name that is equal to the question, and a value that is equal to the answer.
-                $('#container').append("<input type= 'radio' name='question- " + i + " " + "' value= '" + questions[i].answers[j] + "' >" + questions[i].answers[j]);
+// $(document).ready(function () {
+//     //Start button to kick everything off
+$('#startButton').on('click', function () {
+    game.start();
+})
 
-            }
-
-        }
-        console.log("It's working");
-    })
-});
-
-// Array for questions, 
+// // Array for questions, 
 var questions = [{
-    question: 'What letter grade you would give this Triva game?',
+    question: 'What letter grade would you give this Triva game?',
     answers: [" A ", " A ", ' A ', ' A '],
     rightAnswer: ' A '
 }, {
@@ -53,7 +38,49 @@ var questions = [{
     answers: [" A pack ", " A mob ", ' A pride ', ' A flock '],
     rightAnswer: ' A pride '
 }, {
-    question: 'The Statue of Liberty was a gift to commemorate what specific group?',
+    question: 'The Statue of Liberty was a gift to what specific group of Americans?',
     answers: [" Military Troops ", " American Patriots ", ' Hippies ', ' Slaves '],
     rightAnswer: ' Slaves '
 }];
+
+var game = {
+    right: 0,
+    wrong: 0,
+    counter: 90,
+    // function for counting down timer, decreases the timer count by 
+    countdown: function () {
+        game.counter--;
+        $('#counter').html(game.counter);
+        if (game.counter <= 0) {
+            console.log('Youre out of time!!');
+            game.done();
+        }
+    },
+    start: function () {
+        //removes start button once pressed
+        $('#startButton').remove();
+        // Displays timer after button is pressed and removed
+        $("#container").prepend('<h2> Clock\'s ticking: <span id= "counter">90</span></h2>');
+
+        // timer in terms of 1 second
+        timer = setInterval(game.countdown, 1000);
+        // for loop that runs through the questions array
+        for (var i = 0; i < questions.length; i++) {
+            // Takes the question from the questions array and appends them displaying them in a header
+            $('#container').append('<h2>' + questions[i].question + '</h2>');
+            // for loop that runs through the answers from their respective questions index
+            for (var j = 0; j < questions[i].answers.length; j++) {
+                //  append each answer with a radio type,  a name that is equal to the question, and a value that is equal to the answer.
+                $('#container').append("<input type= 'radio' name='question- " + i + " " + "' value= '" + questions[i].answers[j] + "' >" + questions[i].answers[j]);
+
+            }
+
+        }
+    }
+}       //console.log("It's working");
+
+
+
+
+
+
